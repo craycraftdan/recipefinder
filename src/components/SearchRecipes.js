2272930c1f050ee066bf64e2f124b7869d9b02d9
Craycraft <Dan.Craycraft@lids.com>
2017-08-04 16:07:00 -0400

    init comit

---
+++ src/components/SearchRecipes.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { setRecipes } from '../actions'
+
+class SearchRecipes extends Component {
+    constructor(){
+        super()
+        this.state = {
+            ingredients: '',
+            dish: ''
+        }
+    }
+
+    search(){
+        let { ingredients, dish } = this.state  //es6 desctruioning syntax
+        const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`
+        fetch(url, {
+            method: 'GET'
+        }).then(response => response.json())
+          .then(json => {
+              this.props.setRecipes(json.results)
+          })
+    }
+
+    render(){
+        return(
+            <Form inline>
+                <FormGroup>
+                    <ControlLabel>Ingredients</ControlLabel>
+                    {' '}
+                    <FormControl 
+                        type='text' 
+                        placeholder='Garlic, Chicken...' 
+                        onChange={event => this.setState({ingredients: event.target.value})}
+                    />
+                </FormGroup>
+                {' '}
+                <FormGroup>
+                    <ControlLabel>Dish</ControlLabel>
+                    {' '}
+                    <FormControl 
+                        type='text' 
+                        placeholder='Abodo' 
+                        onChange={event => this.setState({dish: event.target.value})}    
+                    />
+                </FormGroup>
+                <Button onClick={() => this.search()}>Submit</Button>
+            </Form>
+        )
+    }
+}
+
+
+export default connect(null, { setRecipes })(SearchRecipes)
